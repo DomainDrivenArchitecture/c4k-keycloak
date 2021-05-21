5eafc858447b26340e76f6c58261bddd6a0bf0e7
jem <michael.jerger@meissa-gmbh.de>
2021-05-21 18:41:24 +0200

    use verified clojars groupname

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dda/k8s-keycloak "0.1.0-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/k8s-keycloak "0.1.0-SNAPSHOT"
   :description "keycloak k8s-installation package"
-  :url "https://www.domaindrivenarchitecture.org"
+  :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
