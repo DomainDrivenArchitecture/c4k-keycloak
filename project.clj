138bb8f38a2f33e990bfdaf039e801341f0bc693
erik <erik.seiert@meissa-gmbh.de>
2023-01-27 13:02:19 +0100

    Bump c4k-common version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.reader "1.3.6"]
-                 [org.domaindrivenarchitecture/c4k-common-clj "5.0.1"]]
+                 [org.domaindrivenarchitecture/c4k-common-clj "6.0.0"]]
   :target-path "target/%s/"
   :source-paths ["src/main/cljc"
                  "src/main/clj"]
